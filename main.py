28cf69aa307fef49d3f4c43f40f906cdab44a39f
Gaurav Dube <107090129+GD1813@users.noreply.github.com>
2026-08-02 23:48:40 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-#Day1 Sum of two numbers
-
-num1 = int(input("Enter the value of num1: "))
-num2 = int(input("Enter the value of num2: "))
-
-result = num1 + num2
-print(f"The sum of the two numbers is {result}.")
