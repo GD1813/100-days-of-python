2ea37b5c9e3d63aefa8e774021119adf8fa292f7
Gaurav Dube <107090129+GD1813@users.noreply.github.com>
2026-08-04 20:26:31 +0530

    Day 3: Add Positive, Negative, or Zero program
    Added the Day 3 Python program to determine whether a number is positive, negative, or zero using conditional statements. Included the README and output screenshot.

---
+++ Day03_Positive_Negative_or_Zero/main.py
@@ -0,0 +1,10 @@
+#Day 3 – Positive, Negative, or Zero
+
+num = int(input("Enter the number: "))
+
+if num > 0 :
+    print(f"{num} is a positive number.")
+elif num < 0 :
+    print(f"{num} is a negative number.")
+else:
+    print(f"{num} is a zero.")
