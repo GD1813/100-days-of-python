143942373629a5b5c2abcbc17adce4bc1526c90c
Gaurav Dube <107090129+GD1813@users.noreply.github.com>
2026-08-05 21:28:15 +0530

    Day 4: Add Largest of Two Numbers program
    Added the Day 4 Python program to compare two numbers and determine the larger value or identify if both numbers are equal. Included the README and output screenshot.

---
+++ Day04_Largest_of_Two_Numbers/main.py
@@ -0,0 +1,12 @@
+#Day 4 – Largest of Two Numbers
+
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2 :
+    print(f"{num1} is greater than {num2}")
+elif num2 > num1 :
+    print(f"{num2} is greater than {num1}")
+else:
+    print(f"{num1} is equal to {num2}")
